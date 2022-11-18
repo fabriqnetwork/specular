5242ca672f633f69db14d4e1aca119d9da8a5b63
Aditya Sharma <aditya18274@iiitd.ac.in>
2022-11-19 03:43:24 +0530

    Fix: slither key issues
    adding example env file
    
    Fix: example env file
    
    fix: rename example env to .env
    
    Rename example.env to .env
    
    modular hardhat config
    
    Delete .env
    
    Fix: empty private key issues slither

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import "@nomiclabs/hardhat-ethers";
+import {ethers} from "ethers";
 import {HardhatUserConfig} from 'hardhat/types';
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -10,7 +11,49 @@ import '@openzeppelin/hardhat-upgrades';
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+
+const mnemonic =
+  process.env.MNEMONIC ??
+  "test test test test test test test test test test test junk";
+
+const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+const INFURA_KEY = process.env.INFURA_KEY || '';
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
+const SEQUENCER_PRIVATE_KEY = process.env.SEQUENCER_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const SEQUENCER_ADDRESS = process.env.SEQUENCER_ADDRESS ?? wallet.address;
+
+
+function createConfig(network: string) {
+  return {
+    url: getNetworkURL(network),
+    accounts: getNetworkAccounts(network),
+  }
+}
+
+function getNetworkAccounts(network: string) {
+  if (network === 'goerli') {
+    return !!SEQUENCER_PRIVATE_KEY ? [`0x${SEQUENCER_PRIVATE_KEY}`] : {mnemonic};
+  } else {
+    return { mnemonic }
+  }
+}
+
+function getNetworkURL(network: string) {
+  if(network === 'mainnet') {
+    return `https://mainnet.infura.io/v3/${INFURA_KEY}`;
+  } else if (network === 'goerli') {
+    return `https://goerli.infura.io/v3/${INFURA_KEY}`; 
+  } else if (network === 'sepolia') {
+    return `https://sepolia.infura.io/v3/${INFURA_KEY}`;
+  } else if (network === 'gnosis') {
+    return "https://rpc.gnosischain.com/"
+  }
+} 
+
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: {
     version: "0.8.4",
     settings: {
@@ -31,16 +74,15 @@ const config: HardhatUserConfig = {
   namedAccounts: {
     sequencer: {
       default: 0,
-      "goerli": '0xf112347faDA222A95d84626b19b2af1DB6672C18'
+      "goerli": SEQUENCER_ADDRESS,
     },
     validator: 1,
   },
   networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
-      chainId: 5,
-      accounts: [`0x${process.env.SEQUENCER_PRIVATE_KEY}`],
-    },
+    mainnet: createConfig('mainnet'),
+    sepolia: createConfig("sepolia"),
+    goerli: createConfig("goerli"),
+    gnosis: createConfig("gnosis"),
     hardhat: {
       gas: "auto",
       mining: {
