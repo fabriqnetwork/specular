a7bfe6cd5d54238d3dc5d7b9c83cd0a76021d33c
destroyersrt <aditysharma.srt@protonmail.com>
2022-12-13 01:34:46 +0530

    configured eslint for js & ts

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,46 +1,46 @@
-require("dotenv").config();
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
-require('hardhat-abi-exporter');
-require('hardhat-deploy');
-require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config()
+require('@nomiclabs/hardhat-ethers')
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-waffle')
+require('hardhat-abi-exporter')
+require('hardhat-deploy')
+require('@openzeppelin/hardhat-upgrades')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 200
+      }
+    }
   },
   paths: {
-    sources: "./src",
+    sources: './src'
   },
   abiExporter: {
-    path: "./abi",
+    path: './abi',
     runOnCompile: true,
-    clear: true,
+    clear: true
   },
   namedAccounts: {
     sequencer: 0,
-    validator: 1,
+    validator: 1
   },
   networks: {
     hardhat: {
-      gas: "auto",
+      gas: 'auto',
       mining: {
         auto: true,
         interval: 5000,
         mempool: {
-          order: "fifo",
-        },
-      },
-    },
-  },
-};
+          order: 'fifo'
+        }
+      }
+    }
+  }
+}
