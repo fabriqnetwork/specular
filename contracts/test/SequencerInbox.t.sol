cf1b875badf7bb1bd09d527a2bf78ca88e84a7c5
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-14 13:08:45 -0600

    Removed the TODOs from test file

--- contracts/test/SequencerInbox.t.sol
+++ contracts/test/SequencerInbox.t.sol
@@ -82,15 +82,3 @@ contract SequencerInboxTest is BaseSetup {
         seqIn.appendTxBatch(contexts, txLengths, "0x");
     }
 }
-
-/**
- * To-Dos:
- *     1. Write more tests (optimize for well thought-out tests rather than code coverage)
- *     2. Work on Test Harnesses (Harness contracts inherit from the Contracts under Test and expose the internal functions as external ones.)
- *         This is useful in testing internal functions.
- *     3. Workaround functions which expose functionality or information otherwise unavailable in the original smart contract.
- *     4. Make sure we have a healthy mix of Integration tests, Fork Tests, positive & negative unit tests for each code path
- *     5. Ensure that no tainted data (malicious user input) ever reaches a sink (contract logic where something important is happening)
- *     6. Write tests for deployment scripts (assuming they will be written in Solidity)
- *     7. Inspect potential cases for front running (not much relevant in our case)
- */
