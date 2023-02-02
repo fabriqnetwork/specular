e0c329d9bf9325976aa4637ca7f6ab15deb4d10a
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-02-02 22:31:32 +0530

    Initial commit for seqIn tests

--- contracts/test/SequencerInbox.t.sol
+++ contracts/test/SequencerInbox.t.sol
@@ -20,40 +20,31 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
+import "../src/ISequencerInbox.sol";
 import "../src/libraries/Errors.sol";
-import {ISequencerInbox} from "../src/ISequencerInbox.sol";
 import {SequencerInbox} from "../src/SequencerInbox.sol";
 import {Utils} from "./utils/Utils.sol";
 
-contract BaseSetup is Test {
-    Utils internal utils;
-    address payable[] internal users;
-
+contract SequencerBaseSetup is Test {
     address internal sequencer;
     address internal alice;
     address internal bob;
 
     function setUp() public virtual {
-        utils = new Utils();
-        users = utils.createUsers(3);
+        sequencer = makeAddr("sequencer");
 
-        sequencer = users[0];
-        vm.label(sequencer, "Sequencer");
+        alice = makeAddr("alice");
 
-        alice = users[1];
-        vm.label(alice, "Alice");
-
-        bob = users[2];
-        vm.label(bob, "Bob");
+        bob = makeAddr("bob");
     }
 }
 
-contract SequencerInboxTest is BaseSetup {
+contract SequencerInboxTest is SequencerBaseSetup {
     SequencerInbox private seqIn;
+    SequencerInbox private seqIn2;
 
     function setUp() public virtual override {
-        BaseSetup.setUp();
+        SequencerBaseSetup.setUp();
 
         SequencerInbox _impl = new SequencerInbox();
         bytes memory data = abi.encodeWithSelector(SequencerInbox.initialize.selector, sequencer);
@@ -63,10 +54,34 @@ contract SequencerInboxTest is BaseSetup {
         seqIn = SequencerInbox(address(proxy));
     }
 
+    function test_initializeSequencerInbox_withSequencerAddressZero() external {
+        SequencerInbox _impl2 = new SequencerInbox();
+        bytes memory data = abi.encodeWithSelector(SequencerInbox.initialize.selector, address(0));
+        address proxyAdmin = makeAddr("Proxy Admin");
+
+        vm.expectRevert(ZeroAddress.selector);
+        TransparentUpgradeableProxy proxy2 = new TransparentUpgradeableProxy(address(_impl2), proxyAdmin, data);
+
+        seqIn2 = SequencerInbox(address(proxy2));
+    }
+
     function test_SequencerAddress() public {
         assertEq(seqIn.sequencerAddress(), sequencer, "Sequencer Address is not as expected");
     }
 
+    function test_intializeSequencerInbox_cannotBeCalledTwice() external {
+        vm.expectRevert("Initializable: contract is already initialized");
+
+        // Since seqIn has already been initialised once(in the setUp function), if we try to initialize it again, it should fail
+        seqIn.initialize(makeAddr("random address"));
+    }
+
+    function testFail_verifyTxInclusion_withEmptyProof() external {
+        bytes memory emptyProof = bytes("");
+        seqIn.verifyTxInclusion(emptyProof);
+    }
+
+    // Only sequencer can append transaction batches to the sequencerInbox
     function test_RevertWhen_InvalidSequencer() public {
         vm.expectRevert(abi.encodeWithSelector(NotSequencer.selector, alice, sequencer));
         vm.prank(alice);
@@ -81,5 +96,16 @@ contract SequencerInboxTest is BaseSetup {
         uint256[] memory contexts = new uint256[](1);
         uint256[] memory txLengths = new uint256[](1);
         seqIn.appendTxBatch(contexts, txLengths, "0x");
+    }
+
+    /////////////////////////////
+    // appendTxBatch
+    /////////////////////////////
+    function test_appendTxBatch_positiveCase() public {
+        // function appendTxBatch(uint256[] calldata contexts, uint256[] calldata txLengths, bytes calldata txBatch)
+        // To be able to call this function, we first need an array of `contexts`
+        // So, what is `contexts`? => Each context corresponds to a single "L2 block"
+        // `contexts` is represented with uint256 3-tuple: (numTxs, l2BlockNumber, l2Timestamp)
+        // Ok, so it means how many transactions were in that block, the l2 block.number and the l2 block.timestamp of that block
     }
 }
