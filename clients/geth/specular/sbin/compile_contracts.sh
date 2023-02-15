792c177bd5bf04f4bcf38c4515724312d7c5ccf9
destroyersrt <aditysharma.srt@protonmail.com>
2023-02-15 22:15:22 +0530

    removing force

--- clients/geth/specular/sbin/compile_contracts.sh
+++ clients/geth/specular/sbin/compile_contracts.sh
@@ -2,4 +2,4 @@
 SBIN=`dirname $0`
 SBIN="`cd "$SBIN"; pwd`"
 . $SBIN/configure.sh
-cd $CONTRACTS_DIR && yarn install --force && npx hardhat compile
+cd $CONTRACTS_DIR && yarn install && npx hardhat compile
