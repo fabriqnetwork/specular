41162fa1e56e743f7933a21370b1f95e6ac541e7
Zhe Ye <leaferx@outlook.com>
2023-02-15 21:08:52 -0800

    format

--- contracts/scripts/create_genesis.js
+++ contracts/scripts/create_genesis.js
@@ -1,75 +1,89 @@
-const ethers = require('ethers');
-const fs = require('fs');
+const ethers = require("ethers");
+const fs = require("fs");
 const FaucetJson = require("../artifacts/src/pre-deploy/Faucet.sol/Faucet.json");
 const assert = require("assert");
 let GenesisJson;
 
-const createContractObject = (deployedBytecode, contractBalance, storageSlots, valueAtSlots) => {
-    
-    assert(storageSlots.length == valueAtSlots.length, "incorrect storage-values array lengths")
-    
-    let storageSlotsObj = {};
-    for(let i = 0; i < storageSlots.length; i++) {
-        storageSlotsObj[storageSlots[i].toString()] = valueAtSlots[i];
-    }
+const createContractObject = (
+  deployedBytecode,
+  contractBalance,
+  storageSlots,
+  valueAtSlots
+) => {
+  assert(
+    storageSlots.length == valueAtSlots.length,
+    "incorrect storage-values array lengths"
+  );
 
-    const contractObject = {
-        "code": deployedBytecode,
-        "balance": Number(contractBalance).toString(),
-        "storage": storageSlotsObj
-    }
-    return contractObject;
-}
+  let storageSlotsObj = {};
+  for (let i = 0; i < storageSlots.length; i++) {
+    storageSlotsObj[storageSlots[i].toString()] = valueAtSlots[i];
+  }
+
+  const contractObject = {
+    code: deployedBytecode,
+    balance: Number(contractBalance).toString(),
+    storage: storageSlotsObj,
+  };
+  return contractObject;
+};
 
 const createFaucetContractObject = () => {
-    const faucetDeployedBytecode = FaucetJson.deployedBytecode;
-    const faucetBalance = ethers.BigNumber.from("10").pow(20);
-    
-    let storageSlots = [];
-    let valueAtSlots = [];
+  const faucetDeployedBytecode = FaucetJson.deployedBytecode;
+  const faucetBalance = ethers.BigNumber.from("10").pow(20);
 
-    // Storage Slot 0 stores the address of the owner
-    storageSlots[0] = "0x0000000000000000000000000000000000000000000000000000000000000000";
-    // Storage Slot 1 stores the amountAllowed
-    storageSlots[1] = "0x0000000000000000000000000000000000000000000000000000000000000001";
+  let storageSlots = [];
+  let valueAtSlots = [];
 
-    // Owner Address - 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-    valueAtSlots[0] = "0x000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-    // Amount Allowed - 0.01 ETH
-    valueAtSlots[1] = "0x000000000000000000000000000000000000000000000000002386f26fc10000"
-    
-    const faucetObject = createContractObject(faucetDeployedBytecode, faucetBalance, storageSlots, valueAtSlots);
-    return faucetObject;
-}
+  // Storage Slot 0 stores the address of the owner
+  storageSlots[0] =
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+  // Storage Slot 1 stores the amountAllowed
+  storageSlots[1] =
+    "0x0000000000000000000000000000000000000000000000000000000000000001";
+
+  // Owner Address - 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
+  valueAtSlots[0] =
+    "0x000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+  // Amount Allowed - 0.01 ETH
+  valueAtSlots[1] =
+    "0x000000000000000000000000000000000000000000000000002386f26fc10000";
+
+  const faucetObject = createContractObject(
+    faucetDeployedBytecode,
+    faucetBalance,
+    storageSlots,
+    valueAtSlots
+  );
+  return faucetObject;
+};
 
 const main = () => {
+  const inFlagIndex = process.argv.indexOf("--in");
+  let baseGenesisPath;
 
-    const inFlagIndex = process.argv.indexOf("--in");
-    let baseGenesisPath;
-    
-    if(inFlagIndex > -1) {
-        baseGenesisPath = process.argv[inFlagIndex+1];
-        GenesisJson = require(baseGenesisPath);
-    } else {
-        throw new Error("Please specify the base genesis path");
-    }
+  if (inFlagIndex > -1) {
+    baseGenesisPath = process.argv[inFlagIndex + 1];
+    GenesisJson = require(baseGenesisPath);
+  } else {
+    throw new Error("Please specify the base genesis path");
+  }
 
-    const outFlagIndex = process.argv.indexOf("--out");
-    let genesisPath;
+  const outFlagIndex = process.argv.indexOf("--out");
+  let genesisPath;
 
-    if(outFlagIndex > -1) {
-        genesisPath = process.argv[outFlagIndex+1];
-    } else {
-        console.log("Setting out genesis path same as base genesis path");
-        genesisPath = baseGenesisPath;
-    }
+  if (outFlagIndex > -1) {
+    genesisPath = process.argv[outFlagIndex + 1];
+  } else {
+    console.log("Setting out genesis path same as base genesis path");
+    genesisPath = baseGenesisPath;
+  }
 
-    // Address the faucet will be deployed to - address(20)
-    const faucetAddress = "0x0000000000000000000000000000000000000020";
-    
-    GenesisJson.alloc[faucetAddress.toString()] = createFaucetContractObject();
-    fs.writeFileSync(genesisPath, JSON.stringify(GenesisJson, null, 2));
+  // Address the faucet will be deployed to - address(20)
+  const faucetAddress = "0x0000000000000000000000000000000000000020";
 
-}
+  GenesisJson.alloc[faucetAddress.toString()] = createFaucetContractObject();
+  fs.writeFileSync(genesisPath, JSON.stringify(GenesisJson, null, 2));
+};
 
 main();
