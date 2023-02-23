b80df4370403fb4021628a1a925c08a2876c84c3
destroyersrt <aditysharma.srt@protonmail.com>
2023-02-23 16:39:22 +0530

    storage-layout generation

---
+++ contracts/scripts/storage-snapshot.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! command -v forge &> /dev/null
+then
+    echo "forge could not be found. Please install forge by running:"
+    echo "curl -L https://foundry.paradigm.xyz | bash"
+    exit
+fi
+
+echo "$PWD"
+
+contracts=(
+  ../src/AssertionMap.sol:AssertionMap
+  ../src/Rollup.sol:Rollup
+  ../src/SequencerInbox.sol:SequencerInbox
+  ../src/challenge/Challenge.sol:Challenge
+  ../src/challenge/verifier/Verifier.sol:Verifier
+)
+
+dir=$(dirname "$0")
+
+echo "Creating storage layout diagrams.."
+
+echo "=======================" > $dir/../.storage-layout
+echo "👁👁 STORAGE LAYOUT snapshot 👁👁" >> $dir/../.storage-layout
+echo "=======================" >> $dir/../.storage-layout
+
+for contract in ${contracts[@]}
+do
+  echo -e "\n=======================" >> $dir/../.storage-layout
+  echo "➡ $contract">> $dir/../.storage-layout
+  echo -e "=======================\n" >> $dir/../.storage-layout
+  forge inspect --pretty $contract storage-layout >> $dir/../.storage-layout
+done
+echo "Storage layout snapshot stored at $dir/../.storage-layout"
