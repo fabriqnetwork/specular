0d41a76264f6eeabda4f7f683b86bc6601d6f85e
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-03-09 18:26:33 +0530

    Update fmt.go

--- clients/geth/specular/rollup/utils/fmt/fmt.go
+++ clients/geth/specular/rollup/utils/fmt/fmt.go
@@ -13,12 +13,10 @@ type wrapError struct {
 }
 
 func Errorf(format string, args ...interface{}) *wrapError {
-	pc, _, line, _ := runtime.Caller(1)
-	fn := runtime.FuncForPC(pc)
 	err := errors.Errorf(format, args...)
 	return &wrapError{
 		err:  err,
-		msg: fmt.Sprintf("%s | %s:%d", err.ErrorStack(), fn.Name(), line),
+		msg: fmt.Sprintf("%s", err.ErrorStack()),
 	}
 }
 
