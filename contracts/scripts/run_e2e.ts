d6f14da158e292792c79c059fefdaa8abe1ba98c
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-03 01:00:41 +0530

    Update run_e2e.ts

--- contracts/scripts/run_e2e.ts
+++ contracts/scripts/run_e2e.ts
@@ -32,10 +32,12 @@ async function startupContainers(): Promise<void> {
   console.log("Running startupContainers()");
   const { stdout: pwdOutput } = await execAsync("pwd && ls -la");
   console.log(pwdOutput);
-  await execAsync("cd project");
+
+  await execAsync("cd contracts && cd scripts && cd .. && cd .. && cd project");
   const { stdout: pw1dOutput } = await execAsync("pwd && ls -la");
   console.log(pw1dOutput);
-  await execAsync("npx ts-node scripts/docker_start.ts");
+  await execAsync("rm -rf project/specular-datadir/geth/");
+  await execAsync("npx ts-node ../contracts/scripts/docker_start.ts");
   await execAsync(
     "wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh"
   );
