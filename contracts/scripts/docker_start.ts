d320d000cb64b8a4a539415bcdacc4d22b79af7d
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-02 19:14:48 +0530

    Update docker_start.ts

--- contracts/scripts/docker_start.ts
+++ contracts/scripts/docker_start.ts
@@ -1,32 +1,9 @@
-import { exec } from 'child_process';
-import path from 'path';
+import { executeCommand } from "./docker_utils";
+import path from "path";
 
 const ROOT_DIR = path.join(__dirname, "../../");
 const SPECULAR_DATADIR =
   process.env.SPECULAR_DATADIR || path.join(ROOT_DIR, "specular-datadir");
-
-async function executeCommand(command: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const child = exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing command: ${error.message}`);
-        reject(error);
-        return;
-      }
-      resolve();
-    });
-
-    // Log stdout
-    child.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
-
-    // Log stderr
-    child.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-    });
-  });
-}
 
 // Start containers
 async function startContainers() {
