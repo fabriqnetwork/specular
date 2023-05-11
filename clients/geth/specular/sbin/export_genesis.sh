5e2eba6d1759d186e769c903b095956359db1ef1
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-11 17:19:03 +0530

    Changes for Test Reverted

--- clients/geth/specular/sbin/export_genesis.sh
+++ clients/geth/specular/sbin/export_genesis.sh
@@ -3,3 +3,10 @@ SBIN=`dirname $0`
 SBIN="`cd "$SBIN"; pwd`"
 . $SBIN/configure.sh
 . $SBIN/configure_system.sh
+cd $DATA_DIR
+args=(
+    --datadir ./data_sequencer
+    --verbosity 0
+)
+
+$GETH_SPECULAR_DIR/build/bin/geth "${args[@]}" dump 0
