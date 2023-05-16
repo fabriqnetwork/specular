2a252d64bc71d85bbee625b3edbae13906445568
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-17 02:37:42 +0530

    Update 02_deploy_Rollup.ts

--- contracts/deploy/02_deploy_Rollup.ts
+++ contracts/deploy/02_deploy_Rollup.ts
@@ -1,11 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { Manifest } from "@openzeppelin/upgrades-core";
-import { exec } from "child_process";
-import util from "node:util";
-import path from "node:path";
-
-const CLIENT_SBIN_DIR = "../clients/geth/specular/sbin";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, ethers, upgrades, network } = hre;
@@ -13,22 +8,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { sequencer, deployer } = await getNamedAccounts();
   const deployerSigner = await ethers.getSigner(deployer);
   const { provider } = network;
-  const execPromise = util.promisify(exec);
 
   const sequencerInboxProxyAddress = (await deployments.get("SequencerInbox"))
     .address;
   const verifierProxyAddress = (await deployments.get("Verifier")).address;
-
-  const { err, stdout } = await execPromise(
-    path.join(CLIENT_SBIN_DIR, "export_genesis.sh")
-  );
-  const initialVmHash = JSON.parse(stdout).root;
-
-  if (err !== undefined || !initialVmHash) {
-    throw Error("could not export genesis hash", err);
-  }
-
-  console.log("initial VM hash:", initialVmHash);
 
   const rollupArgs = [
     sequencer, // address _vault
@@ -37,10 +20,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     5, // uint256 _confirmationPeriod
     0, // uint256 _challengePeriod
     0, // uint256 _minimumAssertionPeriod
+    1000000000000, // uint256 _maxGasPerAssertion
     0, // uint256 _baseStakeAmount
     0, // uint256 _initialAssertionID
     0, // uint256 _initialInboxSize
-    initialVmHash, // bytes32_initialVMhash
+    "0x744c19d2e8593c97867b3b6a3588f51cd9dbc5010a395cf199be4bbb353848b8", // bytes32 _initialVMhash
   ];
 
   const Rollup = await ethers.getContractFactory("Rollup", deployer);
