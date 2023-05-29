4903907fbcc9b7f384e641ebe9b177a8d33f8cb4
ayushguptacse14 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-29 12:01:49 +0530

    SpecularBridge UI updated (#121)
    * Bridge UI updated
    
    * Bridge UI updated
    
    * Update FAQ.vue
    
    * Update Header.vue
    
    * update lock file
    
    ---------
    
    Co-authored-by: Zhe Ye <leaferx@outlook.com>

---
+++ bridge-frontend/src/components/FAQ.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="faq-container">
+    <h2>Frequently Asked Questions</h2>
+    <ul>
+      <li v-for="entry in faqEntries" :key="entry.question">
+        <div class="question-block" @click="toggleAnswer(entry)">
+          <h3>{{ entry.question }}</h3>
+          <p v-show="entry.showAnswer">{{ entry.answer }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FAQ',
+  data() {
+    return {
+      faqEntries: [
+        {
+          question: 'What is Specular?',
+          answer: 'Specular Network is a Layer 2 scaling solution for Ethereum that uses optimistic rollup technology to scale the Ethereum network. It is still under development, but it has the potential to provide a number of benefits for Ethereum users, including scalability, security, and decentralization.',
+          showAnswer: false,
+        },
+        {
+          question: 'What is Specular Bridge?',
+          answer: 'The Specular Network Bridge is a two-way bridge that allows users to transfer assets between Ethereum and Specular Network. It is still in beta, but it is expected to be released to the public in the near future. The bridge offers low fees, high throughput, and security.',
+          showAnswer: false,
+        },
+        {
+          question: 'What chains are supported By Specular Bridge?',
+          answer: 'As of today, Specular Network only supports Chiado. However, the team has plans to support other chains in the future.',
+          showAnswer: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleAnswer(entry) {
+      entry.showAnswer = !entry.showAnswer;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.faq-container {
+  padding-top: 50px;
+  padding-bottom: 10px;
+}
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.question-block {
+  background-color: #f2f2f2;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  border-radius: 8px;
+}
+
+h3 {
+  font-size: 18px;
+  margin-bottom: 5px;
+}
+
+p {
+  font-size: 14px;
+  margin-bottom: 10px;
+}
+</style>
--- bridge-frontend/src/components/Footer.vue
+++ bridge-frontend/src/components/Footer.vue
@@ -1,9 +1,111 @@
 <template>
-  <v-footer app>
-    <v-row align="center" justify="center">
-      <v-col cols="12" class="text-center">
-        <span class="white--text">Specular Team &copy; 2023</span>
+  <v-footer app :class="footer.style">
+    <v-row align="center" justify="center" class="footer-row">
+      <v-col cols="4" v-for="link in footer.links" :key="link.title" class="text-center">
+        <div>
+          <h4 class="footer-heading">{{ link.title }}</h4>
+          <ul class="footer-list">
+            <li v-for="item in link.items" :key="item.label">
+              <a :href="item.to || item.href" class="footer-link" target="_blank">{{ item.label }}</a>
+            </li>
+          </ul>
+        </div>
       </v-col>
     </v-row>
+    <v-col cols="1" class="text-center">
+      <span class="footer-caption">{{ footer.caption }}</span>
+    </v-col>
   </v-footer>
 </template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: 'https://specular.network/docs/getting-started',
+              },
+              {
+                label: 'Community',
+                to: 'https://specular.network/docs/community/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/specularl2',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                href: 'https://medium.com/@SpecularL2',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/specularl2',
+              },
+            ],
+          },
+        ],
+        caption: `Copyright © 2023 Specular.`,
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+.dark {
+  background-color: #333;
+  color: #fff;
+}
+
+.footer-heading {
+  padding: 5px;
+  color: #fff;
+  margin: 0 auto;
+}
+
+.footer-link {
+  color: #ccc;
+  text-decoration: none;
+}
+
+.footer-list {
+  list-style-type: none;
+  padding-left: 0;
+  margin: 0 auto;
+}
+
+.footer-row {
+  padding-top: 5px;
+  padding-bottom: 20px;
+}
+
+.footer-caption {
+  font-size: 12px;
+  opacity: 0.7;
+  color: #fff;
+  margin-top: 10px;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
