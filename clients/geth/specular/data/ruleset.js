47253b5aaccc46a6be273c7e1fe308323c6997b0
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-31 18:31:23 +0530

    Changes based on Review comment

--- clients/geth/specular/data/ruleset.js
+++ clients/geth/specular/data/ruleset.js
@@ -6,7 +6,7 @@ function ApproveTx(r) {
   // TODO: do we need additional / more fine grained rules?
 
   // this file can't access env variables so addresses have to be hardcoded
-  // if (r.transaction.to.toLowerCase() == '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707') {
+  // if (r.transaction.to.toLowerCase() == '0x5fc8d32690cc91d4c39d9d3abcbd16989f875707') {
   //   return 'Approve';
   // }
 
