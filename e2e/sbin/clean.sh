ae17c2fc1336b67a9a8474f6c6f087436c959d83
Zhe Ye <leaferx@outlook.com>
2023-06-01 19:28:13 +0800

    fix clean.sh

--- e2e/sbin/clean.sh
+++ e2e/sbin/clean.sh
@@ -4,9 +4,12 @@
 SBIN_DIR=`dirname $0`
 SBIN_DIR="`cd "$SBIN_DIR"; pwd`"
 PROJECT_DIR=$SBIN_DIR/../project
+PROJECT_DATA_DIR=$PROJECT_DIR/specular-datadir
 DOCKER_DIR=$SBIN_DIR/../../docker
 
 # Clean up
 cd $PROJECT_DIR
 docker compose -f $DOCKER_DIR/docker-compose-integration-test.yml down
-rm -rf $PROJECT_DATA_DIR/geth $PROJECT_DATA_DIR/keystore $PROJECT_DATA_DIR/geth.ipc
+rm -rf $PROJECT_DATA_DIR/geth
+rm -rf $PROJECT_DATA_DIR/keystore
+rm -rf $PROJECT_DATA_DIR/geth.ipc
