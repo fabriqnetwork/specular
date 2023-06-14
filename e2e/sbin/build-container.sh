a45c71c5723d9878fccf05887ebb79d3c1f6dba1
Zhe Ye <leaferx@outlook.com>
2023-06-14 17:03:53 +0800

    remove docker in e2e testing

--- e2e/sbin/build-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define directory structure
-SBIN_DIR=`dirname $0`
-SBIN_DIR="`cd "$SBIN_DIR"; pwd`"
-DOCKER_DIR=$SBIN_DIR/../../docker
-
-# Spin up containers
-docker compose -f $DOCKER_DIR/docker-compose-integration-test.yml build
