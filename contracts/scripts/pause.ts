60355815a31b82f530f672c472499f2bb3c9f5f9
chalkandpaste <chalkandpaste@users.noreply.github.com>
2023-09-04 16:40:32 -0700

    Add Ability to Pause/Unpause Contracts (#147)
    * Pausable contract base implementation
    
    * ability to pause and unpause, no consideration of state (yet)
    
    * basic tests
    
    * more test
    
    * finishing up tests, reviewing bridge because there are no tests there
    
    * add check for consensus
    
    * update OZ, clean up contract inheritance, and get tests running
    
    * test name convention fix
    
    * slither
    
    * remove trailing white space, slither
    
    * remove trailing white space, slither
    
    * remove trailing white space, slither
    
    * Pausable contract base implementation
    
    * ability to pause and unpause, no consideration of state (yet)
    
    * basic tests
    
    * more test
    
    * finishing up tests, reviewing bridge because there are no tests there
    
    * add check for consensus
    
    * update OZ, clean up contract inheritance, and get tests running
    
    * test name convention fix
    
    * slither
    
    * remove trailing white space, slither
    
    * remove trailing white space, slither
    
    * remove trailing white space, slither
    
    * add pause/unpause script
    
    * put Initializable back
    
    * fix script
    
    * trying to sync with github
    
    * sync?
    
    ---------
    
    Co-authored-by: Ol Chalky <chalkandpaste@agave.local>

---
+++ contracts/scripts/pause.ts
@@ -0,0 +1,88 @@
+import * as ethers from "ethers";
+
+const rollupAddr = process.env.ROLLUP_ADDR;
+const rollupPrKey = process.env.ROLLUP_PRIVATE_KEY;
+
+const sequencerAddr = process.env.SEQUENCER_ADDR;
+const sequencerPrKey = process.env.SEQUENCER_PRIVATE_KEY;
+
+const l1BridgeAddr = process.env.L1_BRIDGE_ADDR;
+const l1BridgePrKey = process.env.L1_BRIDGE_PRIVATE_KEY;
+
+const l1OracleAddr = process.env.L1_ORACLE_ADDR;
+const l1OraclePrKey = process.env.L1_ORACLE_PRIVATE_KEY;
+
+const l1PortalAddr = process.env.L1_PORTAL_ADDR;
+const l1PortalPrKey = process.env.L1_PORTAL_PRIVATE_KEY;
+
+const l2BridgeAddr = process.env.L2_BRIDGE_ADDR;
+const l2PortalAddr = process.env.L2_PORTAL_ADDR;
+
+const faucetAddr = process.env.FAUCET_ADDR;
+
+const l1RPCAddr = process.env.L1_RPC_ADDRESS;
+const l1Provider = new ethers.providers.JsonRpcProvider(l1RPCAddr);
+
+const l2RPCAddr = process.env.L2_RPC_ADDRESS;
+const l2Provider = new ethers.providers.JsonRpcProvider(l2RPCAddr);
+
+async function pauseContract(
+  contract: string,
+  address: string,
+  provider: ethers.providers.JsonRpcProvider,
+  privateKey: string
+) {
+  const owner = new ethers.Wallet(privateKey, provider);
+  const factory = await ethers.getContractFactory(contract, address);
+  const c = factory.attach(address);
+  const tx = c.pause();
+  await tx.wait();
+}
+
+async function unpauseContract(
+  contract: string,
+  address: string,
+  provider: ethers.providers.JsonRpcProvider,
+  privateKey: string
+) {
+  const owner = new ethers.Wallet(privateKey, provider);
+  const factory = await ethers.getContractFactory(contract, address);
+  const c = factory.attach(address);
+  const tx = c.unpause();
+  await tx.wait();
+}
+
+async function pauseContracts() {
+  // Pause L1 Contracts
+  pauseContract("Rollup", rollupAddr, l1Provider, rollupPrKey);
+  pauseContract("SequencerInbox", sequencerAddr, l1Provider, sequencerPrKey);
+  pauseContract("L1StandardBridge", l1BridgeAddr, l1Provider, l1BridgePrKey);
+  pauseContract("L1Oracle", l1OracleAddr, l1Provider, l1OraclePrKey);
+  pauseContract("L1Portal", l1PortalAddr, l1Provider, l1PortalPrKey);
+
+  // Pause L2 Contracts
+  pauseContract("L2StandardBridge", l2BridgeAddr, l2Provider, l2BridgePrKey);
+  pauseContract("L2Portal", l2PortalAddr, l2Provider, l2PortalPrKey);
+  pauseContract("Faucet", faucetAddr, l2Provider, faucetPrKey);
+}
+
+async function unpauseContracts() {
+  // Unpause L1 Contracts
+  unpauseContract("Rollup", rollupAddr, l1Provider, rollupPrKey);
+  unpauseContract("SequencerInbox", sequencerAddr, l1Provider, sequencerPrKey);
+  unpauseContract("L1StandardBridge", l1BridgeAddr, l1Provider, l1BridgePrKey);
+  unpauseContract("L1Oracle", l1OracleAddr, l1Provider, l1OraclePrKey);
+  unpauseContract("L1Portal", l1PortalAddr, l1Provider, l1PortalPrKey);
+
+  // Unpause L2 Contracts
+  unpauseContract("L2StandardBridge", l2BridgeAddr, l2Provider, l2BridgePrKey);
+  unpauseContract("L2Portal", l2PortalAddr, l2Provider, l2PortalPrKey);
+  unpauseContract("Faucet", faucetAddr, l2Provider, faucetPrKey);
+}
+
+pauseContracts()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- contracts/src/bridge/L2StandardBridge.sol
+++ contracts/src/bridge/L2StandardBridge.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import {StandardBridge} from "./StandardBridge.sol";
 import {L2Portal} from "./L2Portal.sol";
 import {IMintableERC20} from "./mintable/IMintableERC20.sol";
 import {AddressAliasHelper} from "../vendor/AddressAliasHelper.sol";
 
-contract L2StandardBridge is StandardBridge, Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract L2StandardBridge is StandardBridge {
     using SafeERC20 for IERC20;
 
     L2Portal public L2_PORTAL;
@@ -36,13 +34,13 @@ contract L2StandardBridge is StandardBridge, Initializable, UUPSUpgradeable, Own
     function initialize(address payable _l2Portal, address payable _otherBridge) public initializer {
         L2_PORTAL = L2Portal(_l2Portal);
 
-        __Ownable_init();
-        __UUPSUpgradeable_init();
         __StandardBridge_init(_l2Portal, _otherBridge);
     }
 
+    function _authorizeUpgrade(address) internal override onlyOwner whenPaused {}
+
     /// @inheritdoc StandardBridge
-    receive() external payable override {
+    receive() external payable override whenNotPaused {
         _initiateBridgeETH(msg.sender, msg.sender, msg.value, RECEIVE_DEFAULT_GAS_LIMIT, bytes(""));
     }
 
@@ -100,6 +98,4 @@ contract L2StandardBridge is StandardBridge, Initializable, UUPSUpgradeable, Own
             )
         );
     }
-
-    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
