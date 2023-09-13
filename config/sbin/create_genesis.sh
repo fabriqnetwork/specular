7f6452a7a9ee34c96496dff145bd996f9148c110
Ujval Misra <misraujval@gmail.com>
2023-09-13 10:31:43 +0200

    Revert "init new project structure (#144)"
    This reverts commit 164c28d16b89901551521a27710a9ae772bfd212.

--- config/sbin/create_genesis.sh
+++ config/sbin/create_genesis.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")" && cd ..
 
 pwd
-npx ts-node src/create_genesis.ts --in data/base_genesis.json --out ../services/el_clients/geth/specular/data/genesis.json
+npx ts-node src/create_genesis.ts --in data/base_genesis.json --out ../clients/geth/specular/data/genesis.json
--- lib/el_golang_lib/go.mod
+++
@@ -1,3 +0,0 @@
-module el_golang_lib
-
-go 1.20
