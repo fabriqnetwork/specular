bbd28a56e64e6b4bf4af041d3165ad53cc0054d8
Ol Chalky <chalkandpaste@agave.lan>
2023-09-25 16:12:51 -0700

    changing directory structure, --no-verify

---
+++ sbin/configure.sh
@@ -0,0 +1,9 @@
+# Define directory structure for other scripts.
+SBIN=`dirname $0`
+SBIN="`cd "$SBIN"; pwd`"
+
+export ROOT_DIR=$SBIN/../
+export CONTRACTS_DIR=$ROOT_DIR/contracts
+export DATA_DIR=$ROOT_DIR/data
+export GETH_DIR=$ROOT_DIR/services/el_client/go-ethereum
+export SIDECAR_DIR=$ROOT_DIR/services/sidecar
