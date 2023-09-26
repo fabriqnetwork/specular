75428830520f4676f6e7e05060e6f7b548e6cbce
Ol Chalky <chalkandpaste@agave.lan>
2023-09-26 16:30:19 -0700

    remove old files

--- services/sidecar/sbin/attach_sequencer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SBIN=`dirname $0`
-SBIN="`cd "$SBIN"; pwd`"
-. $SBIN/configure.sh
-cd $DATA_DIR
-$GETH_SPECULAR_DIR/build/bin/geth --datadir ./data_sequencer attach http://localhost:4011
