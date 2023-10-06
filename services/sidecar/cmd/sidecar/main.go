d70d2af40142897ce454ad2a857b4f6e13f7e810
Ol Chalky <chalkandpaste@agave.lan>
2023-10-05 20:38:03 -0700

    getting acocunt manager in order

--- services/sidecar/cmd/sidecar/main.go
+++ services/sidecar/cmd/sidecar/main.go
@@ -13,10 +13,74 @@ import (
 	"github.com/specularL2/specular/services/sidecar/utils/log"
 )
 
+func initializeAccountManager() *accounts.AccountManager {
+	acctMgr := accounts.NewManager(&accounts.Config{InsecureUnlockAllowed: false})
+
+	// < copied from go-ethereum/cmd/config.go:setAccountManagerBackends >
+	keydir := stack.KeyStoreDir()
+	scryptN := keystore.StandardScryptN
+	scryptP := keystore.StandardScryptP
+	if conf.UseLightweightKDF {
+		scryptN = keystore.LightScryptN
+		scryptP = keystore.LightScryptP
+	}
+
+	// Assemble the supported backends
+	if len(conf.ExternalSigner) > 0 {
+		log.Info("Using external signer", "url", conf.ExternalSigner)
+		if extapi, err := external.NewExternalBackend(conf.ExternalSigner); err == nil {
+			am.AddBackend(extapi)
+			return nil
+		} else {
+			return fmt.Errorf("error connecting to external signer: %v", err)
+		}
+	}
+
+	// For now, we're using EITHER external signer OR local signers.
+	// If/when we implement some form of lockfile for USB and keystore wallets,
+	// we can have both, but it's very confusing for the user to see the same
+	// accounts in both externally and locally, plus very racey.
+	am.AddBackend(keystore.NewKeyStore(keydir, scryptN, scryptP))
+	if conf.USB {
+		// Start a USB hub for Ledger hardware wallets
+		if ledgerhub, err := usbwallet.NewLedgerHub(); err != nil {
+			log.Warn(fmt.Sprintf("Failed to start Ledger hub, disabling: %v", err))
+		} else {
+			am.AddBackend(ledgerhub)
+		}
+		// Start a USB hub for Trezor hardware wallets (HID version)
+		if trezorhub, err := usbwallet.NewTrezorHubWithHID(); err != nil {
+			log.Warn(fmt.Sprintf("Failed to start HID Trezor hub, disabling: %v", err))
+		} else {
+			am.AddBackend(trezorhub)
+		}
+		// Start a USB hub for Trezor hardware wallets (WebUSB version)
+		if trezorhub, err := usbwallet.NewTrezorHubWithWebUSB(); err != nil {
+			log.Warn(fmt.Sprintf("Failed to start WebUSB Trezor hub, disabling: %v", err))
+		} else {
+			am.AddBackend(trezorhub)
+		}
+	}
+	if len(conf.SmartCardDaemonPath) > 0 {
+		// Start a smart card hub
+		if schub, err := scwallet.NewHub(conf.SmartCardDaemonPath, scwallet.Scheme, keydir); err != nil {
+			log.Warn(fmt.Sprintf("Failed to start smart card hub, disabling: %v", err))
+		} else {
+			am.AddBackend(schub)
+		}
+	}
+
+	// </ copied from go-ethereum/cmd/config.go:setAccountManagerBackends >
+
+
+}
+
 func startService(cliCtx *cli.Context) error {
 
 	cfg := ParseSystemConfig(cliCtx)
-	acctMgr := _
+
+	// TODO: verify if hard-coded false is a problem
+	acctMgr := initializeAccountManager()
 
 	var disseminator *disseminator.BatchDisseminator
 	var validator *validator.Validator
