a44f9766ccf40147f3784bf455cff484ee7a1f40
Ujval Misra <misraujval@gmail.com>
2023-11-01 16:08:39 -0400

    clean scripts

---
+++ sbin/clean_sp_geth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Check that a dotenv exists.
+GETH_ENV=".sp_geth.env"
+if ! test -f $GETH_ENV; then
+    echo "expected dotenv at ./$GETH_ENV (does not exist); could not clean cwd."
+    exit
+fi
+. $GETH_ENV
+echo "Removing sp-geth data dir $DATA_DIR"
+rm -rf $DATA_DIR
