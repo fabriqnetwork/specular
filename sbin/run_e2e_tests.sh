9383d7feb262c2f709e39857cc2a644a8bf5906f
Ujval Misra <misraujval@gmail.com>
2023-11-29 04:13:43 -0500

    e2e script

--- sbin/run_e2e_tests.sh
+++ sbin/run_e2e_tests.sh
@@ -13,6 +13,14 @@ if ! test -f "$PATHS_ENV"; then
 fi
 echo "Using paths dotenv: $PATHS_ENV"
 . $PATHS_ENV
+# Use sidecar dotenv (to get l1 endpoint)
+SIDECAR_ENV=".sidecar.env"
+if ! test -f "$SIDECAR_ENV"; then
+    echo "Expected dotenv at $SIDECAR_ENV (does not exist)."
+    exit
+fi
+echo "Using sidecar dotenv: $SIDECAR_ENV"
+. $SIDECAR_ENV
 
 ###### PID handling ######
 trap ctrl_c INT
@@ -37,39 +45,40 @@ function ctrl_c() {
 
 ##########################
 
+WORKSPACE_DIR=./workspace-test
+mkdir -p $WORKSPACE_DIR
+cd $WORKSPACE_DIR
+
+echo "Cleaning $WORKSPACE_DIR"
 $SBIN/clean.sh
 # Copy config files to cwd.
 echo "Copying local_devnet config files to cwd..."
-cp -a $CONFIG/deployments/local_devnet/. .
+cp -a $CONFIG_DIR/local_devnet/. .
 
-# Use sidecar .env (to get l1 endpoint)
-ENV=".sidecar.env"
-echo "Using dotenv: $ENV"
-. $ENV
-# Parse url into host:port for wait-for-it.sh
-L1_HOST_AND_PORT=${L1_ENDPOINT#*://}
-
-# TODO: improve logs accross these scripts
 # Start L1
-$SBIN/start_l1.sh &
+$SBIN/start_l1.sh -d &
 L1_PID=$!
-PIDS+=$L1_PID
+PIDS+=($L1_PID)
 
+# Parse url into host:port
+L1_HOST_AND_PORT=${L1_ENDPOINT#*://}
+# Wait for services
+$SBIN/wait-for-it.sh -t 60 $L1_HOST_AND_PORT | sed "s/^/[WAIT] /"
+echo "L1 endpoint is available"
+
+# TODO: remove
+echo "sleeping for a bit"
+sleep 60
+
+# Start sp-geth
+$SBIN/start_sp_geth.sh -c &
+SP_GETH_PID=$!
+PIDS+=($SP_GETH_PID)
+#
 # Start sidecar
 $SBIN/start_sidecar.sh &
 SIDECAR_PID=$!
-PIDS+=$SIDECAR_PID
-# Start sp-geth
-$SBIN/start_sp_geth.sh &
-SP_GETH_PID=$!
-PIDS+=$SP_GETH_PID
-
-# Wait for services
-$SBIN/wait-for-it.sh -t 60 $L1_HOST_AND_PORT | sed "s/^/[WAIT] /"
-$SBIN/wait-for-it.sh -t 60 $L1_HOST_AND_PORT | sed "s/^/[WAIT] /"
-
-cd $CONTRACTS_DIR
-npx hardhat deploy --network specularLocalDev | sed "s/^/[L2 deploy] /"
+PIDS+=($SIDECAR_PID)
 
 # Run testing script
 case $1 in
