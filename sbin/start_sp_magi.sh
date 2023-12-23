90eb945cebf5828f525db542fd5984f3a32cb9e7
Richard Hoop <wrhoop@gmail.com>
2023-12-23 09:05:25 +0000

    mago changes

--- sbin/start_sp_magi.sh
+++ sbin/start_sp_magi.sh
@@ -55,7 +55,7 @@ FLAGS="
     --l2-rpc-url $L2_RPC_URL \
     --sync-mode $SYNC_MODE \
     --l2-engine-url $L2_ENGINE_URL \
-    --jwt-file $JWT_SECRET_PATH \
+    --jwt-secret $JWT_SECRET \
     --rpc-port $RPC_PORT \
     $SYNC_FLAGS $DEVNET_FLAGS $SEQUENCER_FLAGS $@"
 
