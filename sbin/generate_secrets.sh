85ab8159cde7510c56e69b09d3a64d574aaf8285
Ujval Misra <misraujval@gmail.com>
2023-12-28 15:15:55 -0500

    script fixes

--- sbin/generate_secrets.sh
+++ sbin/generate_secrets.sh
@@ -35,10 +35,10 @@ VALIDATOR_ADDRESS=$(generate_wallet $VALIDATOR_PK_PATH)
 echo "Generated account (address=$VALIDATOR_ADDRESS, priv_key_path=$VALIDATOR_PK_PATH)"
 SEQUENCER_ADDRESS=$(generate_wallet $SEQUENCER_PK_FILE)
 echo "Generated account (address=$SEQUENCER_ADDRESS, priv_key_path=$SEQUENCER_PK_FILE)"
-if [ "$DISSEMINATOR_PK_PATH" != "$SEQUENCER_PK_FILE" ]; then
-  echo "$DISSEMINATOR_PK_PATH" "$SEQUENCER_PK_FILE"
+if [ $(realpath "$DISSEMINATOR_PK_PATH") != $(realpath "$SEQUENCER_PK_FILE") ]; then
   guard_overwrite $DISSEMINATOR_PK_PATH
-  cat $SEQUENCER_PK_FILE >$DISSEMINATOR_PK_PATH
+  echo "Copying sequencer private key to $DISSEMINATOR_PK_PATH"
+  cp $SEQUENCER_PK_FILE $DISSEMINATOR_PK_PATH
 fi
 
 # Write dotenv
