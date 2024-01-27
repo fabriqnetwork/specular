0be1152a26f9e9f7b7b56724e5cf4a432110a8e8
Richard Hoop <richard@specular.network>
2024-01-27 01:17:53 -0600

    Renamed SBIN to SCRIPT_DIR for clarity. Renamed JWT to JWT_SECRET for clarity. Removed unnecessary variable reassignment. Removed debugging statement. Improved variable naming for better readability.

--- sbin/generate_jwt_secret.sh
+++ sbin/generate_jwt_secret.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-SBIN=$(dirname "$(readlink -f "$0")")
-SBIN="$(
-  cd "$SBIN"
+
+set -e
+
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SCRIPT_DIR="$(
+  cd "$SCRIPT_DIR"
   pwd
 )"
-. $SBIN/utils/utils.sh
-. $SBIN/utils/crypto.sh
+# Sourcing utility scripts
+. $SCRIPT_DIR/utils/utils.sh
+. $SCRIPT_DIR/utils/crypto.sh
 
-ROOT_DIR=$SBIN/..
-JWT=$(generate_jwt_secret)
-# Write to sp-magi's expected JWT secret path.
-reqdotenv "sp_magi" ".sp_magi.env"
-echo $JWT >$JWT_SECRET_PATH
-# Write to sp-geth's expected JWT secret path.
-reqdotenv "sp_magi" ".sp_geth.env"
-echo $JWT >$JWT_SECRET_PATH
+# Setting the root directory
+ROOT_DIR=$SCRIPT_DIR/..
+
+# Generating JWT secret
+JWT_SECRET=$(generate_jwt_secret)
+
+# Write JWT secret to sp-magi's expected path
+require_dotenv "sp_magi" ".sp_magi.env"
+echo $JWT_SECRET >$JWT_SECRET_PATH
+
+# Write JWT secret to sp-geth's expected path
+require_dotenv "sp_magi" ".sp_geth.env"
+echo $JWT_SECRET >$JWT_SECRET_PATH
