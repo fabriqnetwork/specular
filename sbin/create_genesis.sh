4834e71619690549239b1ce665643e83b1ee16cf
Richard Hoop <richard@specular.network>
2024-01-27 01:12:17 -0600

    In this refactoring, I've standardized the variable names, removed the debugging statement, and improved the readability of the code by reformatting the comments and aligning the code blocks.

--- sbin/create_genesis.sh
+++ sbin/create_genesis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-# Currently the local sbin paths are relative to the project root.
+
+# Set the SBIN variable to the directory of the script
 SBIN=$(dirname "$(readlink -f "$0")")
 SBIN="$(
   cd "$SBIN"
@@ -9,21 +10,26 @@ SBIN="$(
 . $SBIN/utils/utils.sh
 ROOT_DIR=$SBIN/..
 
-# Check that the all required dotenv files exists.
-reqdotenv "paths" ".paths.env"
-reqdotenv "genesis" ".genesis.env"
-reqdotenv "contracts" ".contracts.env"
-reqdotenv "deployments" ".deployments.env"
+# Check that all required dotenv files exist
+require_dotenv "paths" ".paths.env"
+require_dotenv "genesis" ".genesis.env"
+require_dotenv "contracts" ".contracts.env"
+require_dotenv "deployments" ".deployments.env"
 
+# Output the ops directory being used
 echo "Using $OPS_DIR as ops directory."
+
 # Get relative paths for $OPS_DIR
-GENESIS_CFG_PATH=$(relpath $GENESIS_CFG_PATH $OPS_DIR)
-GENESIS_PATH=$(relpath $GENESIS_PATH $OPS_DIR)
-GENESIS_EXPORTED_HASH_PATH=$(relpath $GENESIS_EXPORTED_HASH_PATH $OPS_DIR)
+GENESIS_CFG_PATH=$(relative_path $GENESIS_CFG_PATH $OPS_DIR)
+GENESIS_PATH=$(relative_path $GENESIS_PATH $OPS_DIR)
+GENESIS_EXPORTED_HASH_PATH=$(relative_path $GENESIS_EXPORTED_HASH_PATH $OPS_DIR)
+
+# Generate new genesis file and export hash
 echo "Generating new genesis file at $GENESIS_PATH and exporting hash to $GENESIS_EXPORTED_HASH_PATH"
 cd $OPS_DIR
-guard_overwrite $GENESIS_PATH $AUTO_ACCEPT
-# Create genesis.json file.
+confirm_overwrite $GENESIS_PATH $AUTO_ACCEPT
+
+# Create genesis.json file
 FLAGS=(
   "--genesis-config $GENESIS_CFG_PATH"
   "--out $GENESIS_PATH"
@@ -34,7 +40,7 @@ FLAGS=(
   "--alloc $SEQUENCER_ADDRESS,$VALIDATOR_ADDRESS,$DEPLOYER_ADDRESS"
 )
 
-# hoop: I don't have the patience rn to determine why this isn't being sourced
+# Choose the correct genesis binary and run it
 if [[ -z ${OPS_GENESIS_BIN+x} ]]; then
   CMD="/usr/local/bin/genesis ${FLAGS[@]}"
 else
@@ -44,14 +50,14 @@ fi
 echo "Running $CMD"
 eval $CMD
 
-# Initialize a reference to the config files at
-# "contracts/.genesis" (using relative paths as appropriate).
+# Initialize a reference to the config files at "contracts/.genesis" using relative paths as appropriate
 CONTRACTS_ENV=$CONTRACTS_DIR/.genesis.env
-guard_overwrite $CONTRACTS_ENV $AUTO_ACCEPT
-# Write file, using relative paths.
+confirm_overwrite $CONTRACTS_ENV $AUTO_ACCEPT
+
+# Write file using relative paths
 echo "Initializing contracts genesis dotenv $CONTRACTS_ENV"
-GENESIS_PATH=$(relpath $GENESIS_PATH $CONTRACTS_DIR)
-GENESIS_EXPORTED_HASH_PATH=$(relpath $GENESIS_EXPORTED_HASH_PATH $CONTRACTS_DIR)
-BASE_ROLLUP_CFG_PATH=$(relpath $BASE_ROLLUP_CFG_PATH $CONTRACTS_DIR)
+GENESIS_PATH=$(relative_path $GENESIS_PATH $CONTRACTS_DIR)
+GENESIS_EXPORTED_HASH_PATH=$(relative_path $GENESIS_EXPORTED_HASH_PATH $CONTRACTS_DIR)
+BASE_ROLLUP_CFG_PATH=$(relative_path $BASE_ROLLUP_CFG_PATH $CONTRACTS_DIR)
 echo GENESIS_PATH=$GENESIS_PATH >>$CONTRACTS_ENV
 echo GENESIS_EXPORTED_HASH_PATH=$GENESIS_EXPORTED_HASH_PATH >>$CONTRACTS_ENV
