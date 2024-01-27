d57661e99c7769ef60b270ac7c0832dbbfc29883
Richard Hoop <richard@specular.network>
2024-01-27 01:33:23 -0600

    Standardized variable names and improved readability Removed debugging statements Added comments for clarity Standardized the flag parsing section Made the script more readable and maintainable

--- sbin/start_sp_geth.sh
+++ sbin/start_sp_geth.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 set -e
 
-# currently the local sbin paths are relative to the project root
+# Set SBIN to the absolute path of the script's directory
 SBIN=$(dirname "$(readlink -f "$0")")
 SBIN="$(
   cd "$SBIN"
   pwd
 )"
+# Source the utility functions
 . $SBIN/utils/utils.sh
 ROOT_DIR=$SBIN/..
 
-# Check that the all required dotenv files exists.
-reqdotenv "paths" ".paths.env"
-reqdotenv "sp_geth" ".sp_geth.env"
+# Check that the required dotenv files exist
+require_dotenv "paths" ".paths.env"
+require_dotenv "sp_geth" ".sp_geth.env"
 
 # Generate waitfile for service init (docker/k8)
 WAITFILE="/tmp/.${0##*/}.lock"
 
+# If WAIT_DIR is set, use it as the location for the wait file
 if [[ ! -z ${WAIT_DIR+x} ]]; then
   WAITFILE=$WAIT_DIR/.${0##*/}.lock
 fi
 
-# Parse args.
+# Parse options
 optspec="chw"
 while getopts "$optspec" optchar; do
   case "${optchar}" in
@@ -29,16 +31,18 @@ while getopts "$optspec" optchar; do
     WAIT=true
     ;;
   c)
-    echo "Cleaning..."
+    # Remove debugging statement
     $SBIN/clean_sp_geth.sh
     ;;
   h)
+    # Print usage information and exit
     echo "usage: $0 [-c][-h][-w]"
     echo "-c : clean before running"
-    echo "-w : generate docker-compose wait for file"
+    echo "-w : generate docker-compose wait file"
     exit
     ;;
   *)
+    # Handle unknown options
     if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
       echo "Unknown option: '-${OPTARG}'"
       exit 1
@@ -47,6 +51,7 @@ while getopts "$optspec" optchar; do
   esac
 done
 
+# Remove docker wait file if WAIT is set to true
 if [ "$WAIT" = "true" ]; then
   if test -f $WAITFILE; then
     echo "Removing wait file for docker..."
@@ -54,6 +59,7 @@ if [ "$WAIT" = "true" ]; then
   fi
 fi
 
+# Initialize sp-geth with genesis json if necessary
 if [ ! -d $DATA_DIR ]; then
   echo "Initializing sp-geth with genesis json at $GENESIS_PATH"
   if [ ! -f $GENESIS_PATH ]; then
@@ -89,18 +95,22 @@ FLAGS="
 "
 
 echo "Starting sp-geth with the following aruments:"
+# Start sp-geth with specified flags
+echo "Starting sp-geth with the following arguments:"
 echo $FLAGS
-
 $SP_GETH_BIN $FLAGS &
 
 PID=$!
 echo "PID: $PID"
 
+# Wait for sp-geth to start
 sleep 15
 
+# Create wait file for docker if WAIT is set to true
 if [ "$WAIT" = "true" ]; then
   echo "Creating wait file for docker at $WAITFILE..."
   touch $WAITFILE
 fi
 
+# Wait for sp-geth to finish
 wait $PID
