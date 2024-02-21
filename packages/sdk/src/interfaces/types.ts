7ec5d89b46c9bf413f91674b61b5eaa78b13f837
Stefan Velkoski <34687079+StefanVelkoski@users.noreply.github.com>
2024-02-21 16:57:20 +0100

    feat: SDK serviceBridge class implementation (#320)
    * feat: sdk setup initial architecture and utils
    
    * chore: update pnpm lockfile
    
    * fix: update pnpm version
    
    * fix: update pnpm version
    
    * feat: add deposits
    
    * feat: withdrawals and update waitoracleblock
    
    * feat: withdrawals and update waitoracleblock
    
    * feat: finalizeWithdrawal and resolving comments
    
    * fix: remove magi submodule conflicts
    
    * chore: update readme, code refactor, implement getMessageStatus()
    
    * chore: update readme, code refactor, implement getMessageStatus()
    
    * fix: sol linter
    
    * chore: delete logs
    
    * chore: resolve comments
    
    * fix: withdrawal
    
    * fix: withdrawal + helper functions
    
    * chore: update yaml file
    
    * chore: update lock file
    
    * chore: update lockfile
    
    * fix: lockfile
    
    * chore: update pnpm
    
    * fix: pnpm-lock adjustments
    
    * Update pnpm-lock.yaml
    
    * chore: foramt and update magic numbers + configs
    
    * fix: solidity lint
    
    * fix: delete magic number
    
    ---------
    
    Co-authored-by: “StefanVelkoski” <“velkoskis@gmail.com”>

---
+++ packages/sdk/src/interfaces/types.ts
@@ -0,0 +1,95 @@
+import {
+  Provider,
+  TransactionReceipt,
+  TransactionResponse,
+} from "@ethersproject/abstract-provider";
+import { Signer } from "@ethersproject/abstract-signer";
+import { Contract, BigNumber } from "ethers";
+
+/**
+ * L1 network chain IDs.
+ */
+export enum L1ChainID {
+  MAINNET = 1,
+  SEPOLIA = 11155111,
+  HARDHAT_LOCAL = 31337,
+}
+
+/**
+ * Message finalizaton status.
+ */
+export enum MessageStatus {
+  // not ready to be finalized
+  PENDING = 0,
+  // ready to be finalized
+  READY = 1,
+  // already finalized
+  DONE = 2,
+}
+
+/**
+ * L2 network chain IDs.
+ */
+export enum L2ChainID {
+  SPECULAR = 93481,
+  SPECULAR_HARDHAT_LOCAL = 13527,
+}
+
+/**
+ * Stuff that can be coerced into a transaction.
+ */
+export type TransactionLike = string | TransactionReceipt | TransactionResponse;
+
+/**
+ * Stuff that can be coerced into a provider.
+ */
+export type ProviderLike = string | Provider;
+
+/**
+ * Stuff that can be coerced into a signer.
+ */
+export type SignerLike = string | Signer;
+
+/**
+ * Stuff that can be coerced into a signer or provider.
+ */
+export type SignerOrProviderLike = SignerLike | ProviderLike;
+
+/**
+ * Stuff that can be coerced into an address.
+ */
+export type AddressLike = string | Contract;
+
+/**
+ * Stuff that can be coerced into a number.
+ */
+export type NumberLike = string | number | BigNumber;
+
+/**
+ * L1 contract references.
+ */
+export interface L1Contracts {
+  L1Portal: AddressLike;
+  L1StandardBridge: AddressLike;
+  L1Rollup: AddressLike;
+}
+
+/**
+ * L2 contract references.
+ */
+export interface L2Contracts {
+  UUPSPlaceholder: AddressLike;
+  L1Oracle: AddressLike;
+  L2Portal: AddressLike;
+  L2StandardBridge: AddressLike;
+  L1FeeVault: AddressLike;
+  L2BaseFeeVault: AddressLike;
+}
+
+/**
+ * L1 and L2 contracts references.
+ */
+export interface ContractsLike {
+  l1: L1Contracts;
+  l2: L2Contracts;
+}
---
+++ packages/sdk/src/utils/helpers.ts
@@ -0,0 +1,320 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import { BigNumber, Contract } from "ethers";
+import {
+  Provider,
+  TransactionReceipt,
+  TransactionResponse,
+} from "@ethersproject/abstract-provider";
+import { Signer } from "@ethersproject/abstract-signer";
+import {
+  SignerOrProviderLike,
+  ProviderLike,
+  TransactionLike,
+  NumberLike,
+  AddressLike,
+} from "../interfaces";
+
+/**
+ * Get a depositProof for finalization.
+ */
+export async function getDepositProof(
+  portalAddress: string,
+  depositHash: string,
+  blockNumber: string,
+  l1Provider: JsonRpcProvider,
+) {
+  const proof = await l1Provider.send("eth_getProof", [
+    portalAddress,
+    [getStorageKey(depositHash)],
+    blockNumber,
+  ]);
+
+  return {
+    accountProof: proof.accountProof,
+    storageProof: proof.storageProof[0].proof,
+  };
+}
+
+/**
+ * Get a withdrawalProof for finalization.
+ */
+export async function getWithdrawalProof(
+  portalAddress: string,
+  withdrawalHash: string,
+  blockNumber: string,
+  l2Provider: JsonRpcProvider,
+) {
+  const proof = await l2Provider.send("eth_getProof", [
+    portalAddress,
+    [getStorageKey(withdrawalHash)],
+    blockNumber,
+  ]);
+
+  return {
+    accountProof: proof.accountProof,
+    storageProof: proof.storageProof[0].proof,
+  };
+}
+// export async function deployTokenPair(l1Bridger: Wallet, l2Relayer: Wallet) {
+//     const TestTokenFactory = new ethers.ContractFactory(
+//         "TestToken",
+//         l1Bridger,
+//     );
+//     const l1Token = await TestTokenFactory.deploy();
+
+//     const MintableERC20FactoryFactory = new ethers.ContractFactory(
+//         "MintableERC20Factory",
+//         l2Relayer,
+//     );
+//     const mintableERC20Factory = await MintableERC20FactoryFactory.deploy(
+//         addresses.l2StandardBridgeAddress,
+//     );
+//     const deployTx = await mintableERC20Factory.createMintableERC20(
+//         l1Token.address,
+//         "TestToken",
+//         "TT",
+//     );
+//     const deployTxWithLogs = await deployTx.wait();
+//     console.log(deployTxWithLogs);
+//     const deployEvent = mintableERC20Factory.interface.parseLog(
+//         deployTxWithLogs.logs[0],
+//     );
+//     const l2TokenAddr = deployEvent.args.localToken;
+
+//     const MintableERC20Factory = await ethers.getContractFactory(
+//         "MintableERC20",
+//         l2Relayer,
+//     );
+//     const l2Token = MintableERC20Factory.attach(l2TokenAddr);
+
+//     return {
+//         l1Token,
+//         l2Token,
+//     };
+// }
+
+/**
+ * Enforces a milisecond delay.
+ */
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+/**
+ * Gets the storage key.
+ */
+export function getStorageKey(messageHash: string) {
+  return ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(
+      ["bytes32", "uint256"],
+      [messageHash, 0],
+    ),
+  );
+}
+
+/**
+ * Blocking function that only exits once the block relayed by L1Oracle is >= the blockNumber
+ * @param {Contract} l1Oracle - the oracle contract deployed on L2
+ * @param {number} blockNumber - the block we are waiting for
+ */
+export async function waitUntilOracleBlock(
+  l1Oracle: Contract,
+  blockNumber: number,
+) {
+  console.log(`Waiting for L1Oracle to relay block #${blockNumber}...`);
+  let oracleBlockNumber = await l1Oracle.number();
+  while (oracleBlockNumber < blockNumber) {
+    await delay(500);
+    oracleBlockNumber = await l1Oracle.number();
+  }
+}
+
+export async function getCurrentL1Assertion(
+  rollup: Contract,
+): Promise<[number, number]> {
+  return new Promise<[number, number]>((resolve, reject) => {
+    rollup.on(rollup.filters.AssertionConfirmed(), async (id: BigNumber) => {
+      try {
+        const assertionId = id.toNumber();
+        const assertion = await rollup.getAssertion(assertionId);
+        const assertionBlockNum = assertion.blockNum.toNumber();
+        console.log({
+          msg: "AssertionConfirmed",
+          id: assertionId,
+          blockNum: assertionBlockNum,
+        });
+        resolve([assertionId, assertionBlockNum]);
+      } catch (error) {
+        reject(error); // Reject promise if there's an error
+      }
+    });
+  });
+}
+
+/**
+ * Blocking function that only exits once L2 block with blockNum has been confirmed on L1
+ * @param {Contract} rollup - the rollup contract deployed on L1
+ * @param {number} blockNum - number of the L2 block we are waiting on
+ */
+export async function waitUntilBlockConfirmed(
+  rollup: Contract,
+  blockNum: number,
+): Promise<[number, number]> {
+  let confirmedAssertionId: number | undefined = undefined;
+  let confirmedBlockNum: number;
+
+  rollup.on(rollup.filters.AssertionCreated(), () => {
+    console.log("AssertionCreated");
+  });
+
+  rollup.on(rollup.filters.AssertionConfirmed(), async (id: BigNumber) => {
+    const assertionId = id.toNumber();
+    const assertion = await rollup.getAssertion(assertionId);
+    const assertionBlockNum = assertion.blockNum.toNumber();
+
+    console.log({
+      msg: "AssertionConfirmed",
+      id: assertionId,
+      blockNum: assertionBlockNum,
+    });
+    if (!confirmedAssertionId && blockNum <= assertionBlockNum) {
+      // Found the first assertion to confirm block
+      confirmedAssertionId = assertionId;
+      confirmedBlockNum = assertionBlockNum;
+    }
+  });
+
+  console.log(`Waiting for L2 block ${blockNum} to be confirmed...`);
+  while (!confirmedAssertionId) {
+    await delay(500);
+  }
+
+  return [confirmedAssertionId!, confirmedBlockNum!];
+}
+
+// eth_getProof block number param cannot have leading zeros
+// This function hexlifys blockNum and strips leading zeros
+export function hexlifyBlockNum(blockNum: number): string {
+  const hexBlockNum = ethers.utils.hexlify(blockNum);
+  // Check if the string starts with "0x" and contains more than just "0x".
+  if (hexBlockNum.startsWith("0x") && hexBlockNum.length > 2) {
+    let strippedString = "0x";
+
+    // Iterate through the characters of the input string starting from the third character (index 2).
+    for (let i = 2; i < hexBlockNum.length; i++) {
+      if (hexBlockNum[i] !== "0") {
+        strippedString += hexBlockNum.substring(i); // Append the remaining characters.
+        return strippedString;
+      }
+    }
+
+    // If all characters are '0', return "0x0".
+    return "0x0";
+  }
+
+  // If the input is not in the expected format, return it as is.
+  return hexBlockNum;
+}
+
+export const assert = (condition: boolean, message: string): void => {
+  if (!condition) {
+    throw new Error(message);
+  }
+};
+
+/**
+ * Converts a SignerOrProviderLike into a Signer or a Provider. Assumes that if the input is a
+ * string then it is a JSON-RPC url.
+ *
+ * @param signerOrProvider SignerOrProviderLike to turn into a Signer or Provider.
+ * @returns Input as a Signer or Provider.
+ */
+export const toSignerOrProvider = (
+  signerOrProvider: SignerOrProviderLike,
+): Signer | Provider => {
+  if (typeof signerOrProvider === "string") {
+    return new ethers.providers.JsonRpcProvider(signerOrProvider);
+  } else if (Provider.isProvider(signerOrProvider)) {
+    return signerOrProvider;
+  } else if (Signer.isSigner(signerOrProvider)) {
+    return signerOrProvider;
+  } else {
+    throw new Error("Invalid provider");
+  }
+};
+
+/**
+ * Converts a ProviderLike into a Provider. Assumes that if the input is a string then it is a
+ * JSON-RPC url.
+ *
+ * @param provider ProviderLike to turn into a Provider.
+ * @returns Input as a Provider.
+ */
+export const toProvider = (provider: ProviderLike): Provider => {
+  if (typeof provider === "string") {
+    return new ethers.providers.JsonRpcProvider(provider);
+  } else if (Provider.isProvider(provider)) {
+    return provider;
+  } else {
+    throw new Error("Invalid provider");
+  }
+};
+
+/**
+ * Pulls a transaction hash out of a TransactionLike object.
+ *
+ * @param transaction TransactionLike to convert into a transaction hash.
+ * @returns Transaction hash corresponding to the TransactionLike input.
+ */
+export const toTransactionHash = (transaction: TransactionLike): string => {
+  if (typeof transaction === "string") {
+    assert(
+      ethers.utils.isHexString(transaction, 32),
+      "Invalid transaction hash",
+    );
+    return transaction;
+  } else if ((transaction as TransactionReceipt).transactionHash) {
+    return (transaction as TransactionReceipt).transactionHash;
+  } else if ((transaction as TransactionResponse).hash) {
+    return (transaction as TransactionResponse).hash;
+  } else {
+    throw new Error("Invalid transaction");
+  }
+};
+
+/**
+ * Converts a number-like into an ethers BigNumber.
+ *
+ * @param num Number-like to convert into a BigNumber.
+ * @returns Number-like as a BigNumber.
+ */
+export const toBigNumber = (num: NumberLike): BigNumber => {
+  return ethers.BigNumber.from(num);
+};
+
+/**
+ * Converts a number-like into a number.
+ *
+ * @param num Number-like to convert into a number.
+ * @returns Number-like as a number.
+ */
+export const toNumber = (num: NumberLike): number => {
+  return toBigNumber(num).toNumber();
+};
+
+/**
+ * Converts an address-like into a 0x-prefixed address string.
+ *
+ * @param addr Address-like to convert into an address.
+ * @returns Address-like as an address.
+ */
+export const toAddress = (addr: AddressLike): string => {
+  if (typeof addr === "string") {
+    assert(ethers.utils.isAddress(addr), "Invalid address");
+    return ethers.utils.getAddress(addr);
+  } else {
+    assert(ethers.utils.isAddress(addr.address), "Invalid address");
+    return ethers.utils.getAddress(addr.address);
+  }
+};
