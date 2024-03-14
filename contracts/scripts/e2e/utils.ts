83c778285818963b06e3beb14828493d4e6cab01
siosw <git@siosw.net>
2024-03-14 14:21:39 +0100

    prettier

--- contracts/scripts/e2e/utils.ts
+++ contracts/scripts/e2e/utils.ts
@@ -15,20 +15,24 @@ const rollupAddress = process.env.ROLLUP_ADDR || "invalid address";
 
 export async function getSignersAndContracts() {
   const l1Bridger = new ethers.Wallet(
-    process.env.BRIDGER_PRIVATE_KEY || "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+    process.env.BRIDGER_PRIVATE_KEY ||
+      "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
     l1Provider,
   );
   const l2Bridger = new ethers.Wallet(
-    process.env.BRIDGER_PRIVATE_KEY || "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+    process.env.BRIDGER_PRIVATE_KEY ||
+      "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
     l2Provider,
   );
 
   const l1Relayer = new ethers.Wallet(
-    process.env.BRIDGER_PRIVATE_KEY || "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+    process.env.BRIDGER_PRIVATE_KEY ||
+      "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
     l1Provider,
   );
   const l2Relayer = new ethers.Wallet(
-    process.env.BRIDGER_PRIVATE_KEY || "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+    process.env.BRIDGER_PRIVATE_KEY ||
+      "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
     l2Provider,
   );
 
